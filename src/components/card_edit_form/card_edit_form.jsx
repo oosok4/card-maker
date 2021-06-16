0d95c54cef2d310fd8830d7c67cd38706633b3bc
oosok4 <oosok4@naver.com>
2021-06-16 13:06:25 +0900

    function add card update and delete

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Button from '../button/button';
 import ImageFileInput from '../image_file_input/image_file_input';
 import styles from './card_edit_form.module.css';
 
-const CardEditForm = ({card}) => {
+const CardEditForm = ({card, updateCard, deleteCard}) => {
+    const nameRef = useRef();
+    const companyRef= useRef();
+    const themeRef= useRef();
+    const emailRef= useRef();
+    const titleRef= useRef();
+    const messageRef= useRef();
+
     const {
         name,
-        emailname,
         company,
         title, 
         email, 
@@ -15,19 +21,71 @@ const CardEditForm = ({card}) => {
         fileName, 
         fileURL
         } = card;
-    const onSubmit = () => {};
+
+    
+    const onChange = (event) => {
+        if(event.currentTarget == null){
+            return;
+        }
+        event.preventDefault();
+        updateCard({
+            ...card,
+            [event.currentTarget.name] : event.currentTarget.value
+            
+            
+        })
+
+
+    }
+    const onSubmit = () => {
+        deleteCard(card);
+    };
+
     return (
-        <form className={styles.form}>
-            <input className={styles.input} type="text" name ='name' value={name} />
-            <input type="text" name ='company' value={company} />
-            <select className={styles.select} name="theme" value={theme}>
+        <form  className={styles.form}>
+            <input 
+                className={styles.input} 
+                type="text" 
+                name ='name' 
+                value={name} 
+                ref={nameRef}
+                onChange={onChange}
+            />
+            <input 
+                type="text" 
+                name ='company' 
+                value={company} 
+                ref={companyRef} 
+                onChange={onChange}
+            />
+            <select className={styles.select} name="theme" value={theme} ref={themeRef} onChange={onChange}>
                 <option value="light">Light</option>
                 <option value="dark">Dark</option>
                 <option value="colorful">Colorful</option>
             </select>
-            <input className={styles.input} type="text" name ='title' value={title} />
-            <input className={styles.input} type="text" name ='email' value={email} />
-            <textarea className={styles.textarea} name="message" value={message}></textarea>
+            <input 
+                className={styles.input} 
+                type="text" 
+                name ='title' 
+                value={title}  
+                ref={titleRef}
+                onChange={onChange}
+            />
+            <input 
+                className={styles.input} 
+                type="text" 
+                name ='email' 
+                value={email} 
+                ref={emailRef}
+                onChange={onChange}
+            />
+            <textarea 
+                className={styles.textarea} 
+                name="message" 
+                value={message} 
+                ref={messageRef}
+                onChange={onChange}
+            ></textarea>
             <div className={styles.fileInput}>
                 <ImageFileInput/>
             </div>
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -6,71 +6,93 @@ import Header from '../header/header';
 import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
-const Maker = ({authService}) => {
-    const [cards, setCards] = useState([
+const Maker = ({ authService }) => {
+    const [cards, setCards] = useState(
         {
-            id : '1',
-            name : 'Ellie',
-            company : 'samsung',
-            theme : 'dark',
-            title : 'software Engineer',
-            email : 'oosok4@naver.com',
-            message : 'go for it',
-            fileName : 'oosok4',
-            fileURL : 'oosok4.png'
-        },
-        {
-            id : '2',
-            name : 'Ellie2',
-            company : 'samsung',
-            theme : 'light',
-            title : 'software Engineer',
-            email : 'oosok4@naver.com',
-            message : 'go for it',
-            fileName : 'oosok4',
-            fileURL : 'oosok4.png'
-        },
-        {
-            id : '3',
-            name : 'Ellie3',
-            company : 'samsung',
-            theme : 'colorful',
-            title : 'software Engineer',
-            email : 'oosok4@naver.com',
-            message : 'go for it',
-            fileName : 'oosok4',
-            fileURL : null
+            '1': {
+                id: '1',
+                name: 'Ellie',
+                company: 'samsung',
+                theme: 'dark',
+                title: 'software Engineer',
+                email: 'oosok4@naver.com',
+                message: 'go for it',
+                fileName: 'oosok4',
+                fileURL: 'oosok4.png'
+            },
+            '2': {
+                id: '2', name: 'Ellie2',
+                company: 'samsung',
+                theme: 'light',
+                title: 'software Engineer',
+                email: 'oosok4@naver.com',
+                message: 'go for it',
+                fileName: 'oosok4',
+                fileURL: 'oosok4.png'
+            },
+            '3': {
+                id: '3',
+                name: 'Ellie3',
+                company: 'samsung',
+                theme: 'colorful',
+                title: 'software Engineer',
+                email: 'oosok4@naver.com',
+                message: 'go for it',
+                fileName: 'oosok4',
+                fileURL: null
+            }
         }
-    ])
+    )
 
     const history = useHistory();
 
-    const onLogOut = () =>{
+    const onLogOut = () => {
         authService.logout();
     };
 
-    useEffect(()=>{
+    useEffect(() => {
         authService.onAuthChange(user => {
-            if(!user){
+            if (!user) {
                 history.push('/');
             }
-
         })
     })
-
-    const addCard = card => {
-        const updated = [...cards,card]; // 기존의 카드's를 불러온 후 새로 만든 카드를 추가시킨다.
-        setCards(updated);
+    // 카드 수정 및 추가
+    const createOrUpdateCard = card => {
+        //이렇게 해도되지만 cards가 다른 사용자의 의해서 이미 값이 변경 되었을 수 도 있다,
+        // 때문에 이런 문제를 해결하기위해 2번 방식으로 업데이트를 한다.
+        // <<<< 1번방식 >>>>>
+        // const updated = {...cards};
+        // updated[card.id] = card;
+        // setCards(updated)
+        // <<<< 2번방식 >>>>>
+        setCards(cards => {
+            const updated = { ...cards };
+            updated[card.id] = card;
+            return updated;
+        });
+    };
+    // 카드 삭제
+    const deleteCard = card => {
+        setCards(cards => {
+            const updated = { ...cards };
+            delete updated[card.id];
+            return updated;
+        });
     }
 
     return (
-        <section className = {styles.maker}>
-            <Header onLogOut={onLogOut}/>
-                <div className={styles.container}>
-                    <Editor cards = {cards} addCard = {addCard}/>
-                    <Preview cards = {cards}/>
-                </div>
-            <Footer/>
+        <section className={styles.maker}>
+            <Header onLogOut={onLogOut} />
+            <div className={styles.container}>
+                <Editor 
+                    cards={cards} 
+                    addCard={createOrUpdateCard} 
+                    updateCard={createOrUpdateCard} 
+                    deleteCard={deleteCard} />
+                <Preview cards={cards} />
+            </div>
+            <Footer />
         </section>
     )
 };
